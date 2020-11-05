5820b7ef1a44365f1e542c1d61dd10c99334bfad
ryanb099 <ryekicks@gmail.com>
2020-11-04 23:58:12 -0500

    Assignment 6

--- js/calc.js
+++ js/calc.js
@@ -1,60 +1,4 @@
 
-  /*          function getVal()
-            {
-                var balance = parseFloat(document.getElementsByName('loan')[0].value);
-                var interest_rate = parseFloat(document.getElementsByName('interest')[0].value/100);
-                var term= parseInt(document.getElementsByName('loan_term')[0].value);
-                var year =parseFloat(document.getElementsByName('year')[0].value);
-                var div = document.getElementById("result");
-	//in case of a re-calc, clear out the div!
-	            div.innerHTML = "";
-	//validate inputs - display error if invalid, otherwise, display table
-	            var balVal = validateInputs(balance);
-	            var intrVal = validateInputs(interest_rate);
-
-	            if (balVal && intrVal)
-	            {
-		            //Returns div string if inputs are valid
-		            div.innerHTML += loanCalc(balance, interest_rate, term);
-	            }
-	            else
-	            {
-		            //returns error if inputs are invalid
-		            div.innerHTML += "Please Check your inputs and retry - invalid values.";
-                }
-            function loanCalc(balance, interest_rate, term)
-            {
-                var month_rate= interest_rate /12;
-                var payment = balance * (month_rate/(1-Math.pow (1+month_rate, -term)));
-                var table= "Date: " + year + "<br/>" +
-                    "Interest Rate: " + (interest_rate*100) + "<br/>" +
-                    "Principal: " + balance + "<br/>" +
-                    "Balance: " + (payment * term) + "<br/><br/>";
-                table += "<table border= '1'><tr><th>Date #</th><th>Interest</th><th>Principal</th><th>Balance</th>";
-                for (var count= 0; count < term; ++count)
-                {
-                    var int= 0;
-                    var monthlyPrin = 0;
-                    table += "<tr align=center>";
-                    table += "<td>" + (count + 1) + "</td>";
-                    table += "<td> $" + balance + "</td>";
-                    //calc the in-loop interest amount and display
-		            interest = balance * month_rate;
-		            table += "<td> $" + interest + "</td>";
-		//calc the in-loop monthly principal and display
-		            monthlyPrin = payment - interest;
-		            table += "<td> $" + monthlyPrin + "</td>";
-		
-		//end the table row on each iteration of the loop	
-		            table += "</tr>";
-		
-		//update the balance for each loop iteration
-		            balance = balance - monthlyPrin;	
-                }
-                table += "</table>";
-                return table;
-            }
-        } */
         function getValues()
         {
             //button click gets values from inputs
@@ -87,8 +31,8 @@
             var result = "Monthly payment: $" + payment.toFixed(2) + "<br />";
                 
             //add header row for table to return string
-            result += "<table border='1'><tr><th>Date</th><th>Balance</th>" + 
-                "<th>Interest</th><th>Principal</th>";
+            result += "<table border='1'><tr><th>Date</th><th>Interest</th>" + 
+                "<th>Principal</th><th>Balance</th>";
             
             /**
              * Loop that calculates the monthly Loan amortization amounts then adds 
